1390548a63edb76ecaa5ddd8a133c2c9b1dfa040
Angel Beltran <aabeltran5910@gmail.com>
2024-11-05 23:48:48 -0600

    Created crud operations for user

---
+++ app/schemas/schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class UserCreate(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    phone_number: str
+    password: str
+
+class UserPublic(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    phone_number: str
+
+class UserDelete(BaseModel):
+    email: str
+    password: str
+
+class UserUpdate(BaseModel):
+    current_email: str
+    current_password: str
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    email: Optional[str] = None
+    phone_number: Optional[str] = None
+    password: Optional[str] = None
