357379a1509bde3cefa9b3c4b7c7da9ddd642873
Angel Beltran <aabeltran5910@gmail.com>
2025-02-18 20:29:19 -0600

    able to recieve data from websockets and store it in db

--- app/helper/websocketdatahandler.py
+++ app/helper/websocketdatahandler.py
@@ -1,13 +1,21 @@
 from sqlmodel import Session, select
-from models.models import User, AnthropometricMeasurements
-from schemas.schemas import *
+from models.models import MetricBatch, HealthMetrics
 from datetime import datetime
 
-
-def handle_websocket_data(
-    db: Session, 
-    batch: MetricBatch, 
-    metric: HealthMetric
-    ):
+# def handle_websocket_data(
+#     db: Session, 
+#     batch: MetricBatch, 
+#     metric: HealthMetric,
+#     user_id: uuid.UUID
+#     ):
     
+#     batch_data = MetricBatch(
+#         user_id=user_id,
+#         datetime.utcnow()
+#     )
+
+#     metric_data = HealthMetrics(
+#         batch_id=batch_data.id,
+#         metric
+#     )
 
